d8c54a6409bcd0b3262ce58d65e4529df8359d5f
Mykyta_Kurylko <mykyta_kurylko@epam.com>
2024-03-03 17:24:07 +0100

    Pages and Layouts with Next.js

--- src/components/EvaluationCriteriaCounter/index.tsx
+++ src/components/EvaluationCriteriaCounter/index.tsx
@@ -1,35 +1,123 @@
+import clsx from "clsx";
 import React, { useState } from "react";
+import styles from "./styles.module.css";
 
-function EvaluationCriteriaCounter({ criteriaWithPercentages }) {
-  // Initialize counts for each criterion passed via props, along with its percentage
-  const initialCounts = criteriaWithPercentages.reduce((acc, { criterion }) => {
-    acc[criterion] = 0;
-    return acc;
-  }, {});
+interface Criterion {
+  criterion: string;
+  coefficient: number;
+}
 
-  const [counts, setCounts] = useState(initialCounts);
+interface EvaluationCriteriaCounterProps {
+  criteriaWithCoefficients: Criterion[];
+  minGrade?: number;
+}
 
-  // Function to increment count for a criterion
-  const incrementCount = (criterion) => {
-    setCounts((prevCounts) => ({
-      ...prevCounts,
-      [criterion]: prevCounts[criterion] + 1,
-    }));
+const EvaluationCriteriaCounter: React.FC<EvaluationCriteriaCounterProps> = ({
+  criteriaWithCoefficients,
+  minGrade = 60,
+}) => {
+  const initialCheckedState = new Array(criteriaWithCoefficients.length).fill(
+    false
+  );
+  const [checkedState, setCheckedState] =
+    useState<boolean[]>(initialCheckedState);
+
+  const handleOnChange = (position: number) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+
+    setCheckedState(updatedCheckedState);
   };
+
+  const calculateGrade = () => {
+    const totalCoefficient = criteriaWithCoefficients.reduce(
+      (total, item) => total + item.coefficient,
+      0
+    );
+    const checkedCoefficient = criteriaWithCoefficients.reduce(
+      (total, item, index) =>
+        checkedState[index] ? total + item.coefficient : total,
+      0
+    );
+
+    return ((checkedCoefficient / totalCoefficient) * 100).toFixed(2);
+  };
+
+  const resetEvaluation = () => {
+    setCheckedState(initialCheckedState);
+  };
+
+  const grade = calculateGrade();
+  const isSuccess = parseFloat(grade) >= minGrade;
+  const message = isSuccess
+    ? 'Please put a grade and click "Mark as passed" button.'
+    : 'Minimum grade to successfully complete the course - 60%. Please provide review comments, set draft grade and click "Send back for updates" button in the Learn.';
 
   return (
     <div>
-      <h2>Evaluation Criteria Counter</h2>
-      <ul>
-        {criteriaWithPercentages.map(({ criterion, percentage }) => (
-          <li key={criterion}>
-            {criterion} ({percentage}%): {counts[criterion]}{" "}
-            <button onClick={() => incrementCount(criterion)}>Increment</button>
-          </li>
-        ))}
-      </ul>
+      <table className={styles.container}>
+        <thead>
+          <tr>
+            <th>Criteria</th>
+            <th>Coefficient</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {criteriaWithCoefficients.map((item, index) => (
+            <tr key={index}>
+              <td>{item.criterion}</td>
+              <td className={styles.center}>{item.coefficient}</td>
+              <td className={styles.center}>
+                <input
+                  className={styles.checkbox}
+                  id={`custom-checkbox-${index}`}
+                  type="checkbox"
+                  checked={checkedState[index]}
+                  onChange={() => handleOnChange(index)}
+                />
+                <label
+                  className={styles.checkboxLabel}
+                  htmlFor={`custom-checkbox-${index}`}
+                ></label>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className={styles.gradeContainer}>
+        <div className={styles.gradeTable}>
+          <table className={styles.table}>
+            <tbody>
+              <tr className={styles.topic}>
+                <th>Student's grade</th>
+                <td
+                  className={clsx({
+                    [styles.success]: isSuccess,
+                    [styles.danger]: !isSuccess,
+                  })}
+                >
+                  {grade}%
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <button className={styles.btn} onClick={resetEvaluation}>
+            Reset
+          </button>
+        </div>
+        <p
+          className={clsx({
+            [styles.success]: isSuccess,
+            [styles.danger]: !isSuccess,
+          })}
+        >
+          {message}
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default EvaluationCriteriaCounter;
